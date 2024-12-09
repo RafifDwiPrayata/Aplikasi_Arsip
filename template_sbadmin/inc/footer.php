c779f17d83461926ab98a88516bab4df4e0e6aac
Rafif Dwi Prayata <dwirafif60@gmail.com>
2024-12-09 09:19:03 +0700

    Menambahkan file revisi

---
+++ template_sbadmin/inc/footer.php
@@ -0,0 +1,17 @@
+<footer class="py-4 bg-light mt-auto">
+                    <div class="container-fluid">
+                        <div class="d-flex align-items-center justify-content-between small">
+                            <div class="text-muted">Copyright &copy; Salman Template 2024</div>
+                            <div>
+                                <a href="#">Privacy Policy</a>
+                                &middot;
+                                <a href="#">Terms &amp; Conditions</a>
+                            </div>
+                        </div>
+                    </div>
+                </footer>
+            </div>
+        </div>
+        <script src="<?php echo BASE_URL; ?>assets/js/scripts.js"></script>
+    </body>
+</html>
---
+++ template_sbadmin/inc/sidebar.php
@@ -0,0 +1,37 @@
+<div id="layoutSidenav">
+        <div id="layoutSidenav_nav">
+            <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
+                <div class="sb-sidenav-menu">
+                    <div class="nav">
+                        <a class="nav-link" href="<?php echo BASE_URL; ?>pages/dashboard.php">
+                            <div class="sb-nav-link-icon">
+                                <i class="fas fa-tachometer-alt"></i>
+                            </div>
+                            Dashboard
+                        </a>
+                        <a class="nav-link" href="<?php echo BASE_URL; ?>pages/user/user.php">
+                            <div class="sb-nav-link-icon">
+                                <i class="fas fa-user"></i>
+                            </div>
+                            User
+                        </a>
+                        <a class="nav-link" href="<?php echo BASE_URL; ?>pages/ganti_password/ganti_password.php">
+                            <div class="sb-nav-link-icon">
+                                <i class="fas fa-lock"></i>
+                            </div>
+                            Ganti Password
+                        </a>
+                        <a class="nav-link" href="<?php echo BASE_URL; ?>index.php">
+                            <div class="sb-nav-link-icon">
+                                <i class="fas fa-sign-out-alt"></i>
+                            </div>
+                            Logout
+                        </a>
+                    </div>
+                </div>
+                <div class="sb-sidenav-footer">
+                    <div class="small">Logged in as:</div>
+                    Saul
+                </div>
+            </nav>
+        </div>
